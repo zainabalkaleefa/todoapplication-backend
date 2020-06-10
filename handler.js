7b849b281cb56c51a562b733b3c5850a7ddc8a6b
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-06-10 03:08:36 +0100

    Get tasks

--- handler.js
+++ handler.js
@@ -1,18 +1,13 @@
-'use strict';
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
 
-module.exports.tasks = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const app = express();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.use(cors());
+
+app.get("/tasks", function(req, res) {
+  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+module.exports.tasks = serverless(app);
